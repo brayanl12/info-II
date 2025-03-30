2375288186f83ff607d99fe3b00363bb6cb7690f
brayanl12 <brayan.lopezl@udea.edu.co>
2025-03-30 12:53:57 -0500

    Ejercicio G4

--- Lab_01/Ejercicios/main.cpp
+++ Lab_01/Ejercicios/main.cpp
@@ -35,9 +35,18 @@ int ejercicio19();
 /*21.EScriba un programa que pida un caracter C, si es una letra la debe convertir de mayuscula a minuscula y viceversa e imprimirla.*/
 char ejercicio21();
 
+/*23.Escriba un programa que pida dos numeros A y B e imprima en pantalla el minimo comun multiplo entre los dos*/
+int ejercicio23();
+
+/*25.Escriba un programa que pida un numero N e imprima en pantalla la cantidad de digitos N.*/
+int ejercicio25();
+/*27.Escriba un programa que actúe como una calculadora con operacion suma,resta,multiplicacion y division, el usuario debe ingresar los operandos y la operacion a realizar.*/
+double ejercicio27();
+/*29.Escriba un programa que adivine un numero A (entre 0 y 100) seleccionado por el usuario (el numero No se ingresa al programa), el programa imprimira en pantalla un numero B y el usuario usara los simbolos '>', '<' y '=' para indicarle al programa si B es mayor,menor o igual que A. El programa imprimira un nuevo numero B, con base en simbolo ingresdo por el usuario, y repetira el proceso hasta acertar el numero seleccionado por el usuario.*/
+int ejercicio29();
 int main()
 {
-    ejercicio21();
+    ejercicio29();
     return 0;
 }
 
@@ -198,4 +207,119 @@ char ejercicio21()
     cout <<"Letra convertida: "<< c <<endl;
     return 0;
 }
+
+int ejercicio23()
+{
+    int A, B, mcm, min;
+    cout <<"Ingrese numero A:"<<endl;
+    cin >>A;
+    cout <<"Ingrese numero B:"<<endl;
+    cin >>B;
+    if( A < B)
+    {
+        min = A;
+    }
+    else
+    {
+        min = B;
+    }
+
+    for (int i=1; i<=min; i++)
+    {
+        if(A%i ==0 && B%i==0)
+        {
+            int mcd=i;
+            mcm= (A*B)/mcd;
+        }
+    }
+    cout <<"El MCM de "<< A <<" y "<< B <<" es: "<< mcm <<endl;
+    return 0;
+}
+
+int ejercicio25()
+{
+    int D, N, x = 0;
+    cout<<"Ingrese numero N: "<<endl;
+    cin>> N;
+    D=N;
+    while(D!=0)
+    {
+        D= D/10;
+        x++;
+    }
+    cout<< N <<" tiene "<< x <<" digitos."<<endl;
+    return 0;
+}
+
+double ejercicio27()
+{
+    double A, B, resultado;
+    char op;
+    cout<< "Ingresar numero A: "<<endl;
+    cin>>A;
+    cout<< "Ingresar operacion (+,-,*,/): "<<endl;
+    cin>>op;
+    cout<< "Ingresar numero B: "<<endl;
+    cin>>B;
+    switch (op)
+    {
+    case'+':
+        resultado = A + B;
+        break;
+    case'-':
+        resultado = A - B;
+        break;
+    case'*':
+        resultado = A * B;
+        break;
+    case'/':
+        if(B !=0)
+        resultado = A / B;
+        else
+        {
+            cout <<"No se puede dividir entre cero"<<endl;
+            return 1;
+        }
+        break;
+    default:
+        cout <<"El operador no se encuentra."<<endl;
+        return 1;
+    }
+    cout << A << op << B << "="<< resultado <<endl;
+    return 0;
+}
+int ejercicio29()
+{
+    int B, min=0, max=100;
+    char respuesta;
+    cout<<"Piense en un numero (entre 0 y 100) e ingrese '>' si es mayor, '<' es menor o '=' si es igual al numero pensado."<<endl;
+    while(min<=max)
+    {
+        B=(min+max)/2;
+        cout<< "Su numero es: "<< B <<"?"<<endl;
+        cout<< "Responda con (>, <, =): "<< endl;
+        cin>>respuesta;
+
+        if (respuesta == '=')
+        {
+            cout<<"El programa adivino su numero: "<< B << endl;
+            break;
+        }
+        else if (respuesta == '>')
+        {
+            min= B+1;
+        }
+        else if (respuesta == '<')
+        {
+            max= B-1;
+        }
+        else
+        {
+            cout <<"No usaste los operadores de comparacion." <<endl;
+        }
+    }
+
+    return 0;
+}
+
 
